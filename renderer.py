c7b95d5c046f59d8ef2cad9df61b31c5f6a8869f
Martin Putniorz <mputniorz@gmail.com>
2014-06-09 13:06:52 +0200

    License sauce

--- renderer.py
+++ renderer.py
@@ -3,7 +3,7 @@
 import os
 
 import click
-from bs4 import BeautifulSoup
+from lxml.html import fromstring
 from jinja2 import Environment, FileSystemLoader
 from typogrify.filters import typogrify
 
@@ -14,13 +14,14 @@ def render(settings):
     env = Environment(loader=FileSystemLoader(os.path.dirname(__file__)))
     context = {k.upper(): v for k, v in settings['context'].items()}
     html_output = env.get_template(settings['template']).render(**context)
-    text_output = BeautifulSoup(html_output).get_text()
+    text_output = fromstring(html_output).text_content()
     typo_output = typogrify(html_output)
-    if settings.get('mail_render'):
+    if settings.get('html_output'):
         with open(settings['html_output'], 'w') as html:
             html.write(typo_output)
-        with open(settings['text_output'], 'w') as text:
-            text.write(typo_output)
+        if settings.get('text_output'):
+            with open(settings['text_output'], 'w') as text:
+                text.write(text_output)
     else:
         print(text_output)
 
@@ -28,13 +29,10 @@ def render(settings):
 @click.command()
 @click.option('--config', help='Custom config file')
 @click.option('--template', help='Template file')
-@click.option('--output', help='Output file')
-def main(config, template, output):
+def main(config, template):
     settings = load_settings(config)
     if template:
         settings['template'] = template
-    if output:
-        settings['mail_render'] = output
     render(settings)
 
 
