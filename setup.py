a634c613a57e7270eb8d6cbde35b174de980756d
Martin Putniorz <mputniorz@gmail.com>
2014-06-16 10:17:07 +0200

    Project structure changes
    * setup.py
    * Shit is now in newschimp folder
    * bin script

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='NewsChimp',
+    version='0.1',
+    description='Newsletter generator for MailChimp',
+    author='Martin Putniorz',
+    author_email='mputniorz@gmail.cpm',
+    url='https://github.com/sputnikus/newschimp',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Topic :: Communications :: Email',
+    ],
+    long_description=open('README.md').read(),
+    scripts=['bin/newschimp']
+    license='LICENSE',
+)
