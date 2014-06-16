4143dd287c4cf780d8fecb35b0b7106b16ca7ac6
Martin Putniorz <mputniorz@gmail.com>
2014-06-16 17:30:10 +0200

    Install requires

--- setup.py
+++ setup.py
@@ -18,10 +18,17 @@ setup(
         'Programming Language :: Python :: 3',
         'Topic :: Communications :: Email',
     ],
-    #entry_points='''
-    #    [console_scripts]
-    #    newschimp=newschimp.core:main
-    #''',
+    install_requires=[
+        'Jinja2>=2.7',
+        'PyYAML>=3.11',
+        'click>=2.0',
+        'lxml>=3.3',
+        'mailchimp>=2.0',
+        'requests>=2.3',
+        'selenium>=2.42',
+        'smartypants>=1.8',
+        'typogrify>=2.0',
+    ],
     scripts=['bin/chimpgen'],
     license='BSD',
     keywords = ['newsletter', 'mail', 'mailchimp']
