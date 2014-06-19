ab5f858c0e5dd75a385ed09a93e9c304e4d16e47
Martin Putniorz <mputniorz@gmail.com>
2014-06-19 21:27:01 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='NewsChimp',
     packages=['newschimp', 'newschimp.social'],
-    version='0.1.1',
+    version='0.1.2',
     description='Newsletter generator for MailChimp',
     long_description=open('README.rst').read(),
     author='Martin Putniorz',
